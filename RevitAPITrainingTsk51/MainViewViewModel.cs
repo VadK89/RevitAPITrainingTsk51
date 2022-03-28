2cace34a01f38b7db4bcefd36da7bb543907f8b9
Вадим <Vadimk89@mail.ru>
2022-03-29 00:00:15 +0500

    Добавьте файлы проекта.

---
+++ RevitAPITrainingTsk51/MainViewViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using RevitAPITrainingLibraryTsk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingTsk51
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            //для взаимодействия с UI docment
+            _commandData = commandData;
+            //SelectCommand = new DelegateCommand(OnSelectCommand);
+            NumberPipesCommand = new DelegateCommand(OnNumPipeComm);
+            VolumeWallCommand = new DelegateCommand(OnVolWallCom);
+            NumberDoorCommand = new DelegateCommand(OnNumDoorCom);
+        }
+
+        public DelegateCommand NumberPipesCommand { get; }
+        public DelegateCommand VolumeWallCommand { get; }
+        public DelegateCommand NumberDoorCommand { get; }
+
+        private void OnNumDoorCom()
+        {
+            RaiseHideRequest();
+
+            List<Element> oElement = ElementCount.PickObjects(_commandData, BuiltInCategory.OST_Doors);
+
+            TaskDialog.Show("Количество дверей", $"Двери: {oElement.Count}");
+
+            RaiseShowRequest();
+        }
+
+        private void OnVolWallCom()
+        {
+            RaiseHideRequest();
+            double volume = VolWallCount.Volume(_commandData);
+            TaskDialog.Show("Объем стен", $"Объем стен: {volume} ");
+            RaiseShowRequest();
+        }
+
+        private void OnNumPipeComm()
+        {
+            RaiseHideRequest();
+
+            List<Element> oElement = ElementCount.PickObjects(_commandData, BuiltInCategory.OST_PipeCurves);
+
+            TaskDialog.Show("Количество труб", $"Трубы: {oElement.Count}");
+
+            RaiseShowRequest();
+        }
+
+        public event EventHandler HideRequest;
+        //метод для закрытия окна
+        private void RaiseHideRequest()
+        {//Для запуска методов привзязанных к запросу закрытия
+            HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler ShowRequest;
+        //метод для закрытия окна
+        private void RaiseShowRequest()
+        {//Для запуска методов привзязанных к запросу закрытия
+            ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+
+    }
+}
+
---
+++ RevitAPITrainingTsk51/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitAPITrainingTsk51")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevitAPITrainingTsk51")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("73bd8cea-d398-4519-8d46-b169c6c79762")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
